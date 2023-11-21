06c84c49fd24a8afa797c4d416ccbfb8150b8914
cosmo4 <warner.luke0102@gmail.com>
2023-11-21 11:10:09 -0700

    Add search functionality to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
-  
+  let search = '';
+  const searchUrl = import.meta.env.VITE_SEARCH_URL;
+
+    async function handleSearch(event) {
+        event.preventDefault();
+
+        const response = await fetch(`${searchUrl}${search}`);
+        const json = await response.json();
+
+        console.log(json);
+    }
 </script>
 
 <main>
-  
+    <form on:submit={handleSearch}>
+        <label for="search">Search for a recipe</label>
+        <input id="search" name="search" type="text" placeholder="Search..." bind:value={search}>
+        <button type="submit">Search</button>
+    </form>
 </main>
 
 <style>
