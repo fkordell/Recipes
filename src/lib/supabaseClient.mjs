721d14cb1ab10cc797f27f3af1f4750b80cd98b6
cosmo4 <warner.luke0102@gmail.com>
2023-12-05 10:54:55 -0700

    ready to merge into main

--- src/lib/supabaseClient.mjs
+++ src/lib/supabaseClient.mjs
@@ -4,15 +4,17 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey)
-const tableName = 'recipe'
+const tableName = 'post'
 export async function fetchData() {
     const { data, error } = await supabase.from(tableName).select('*');
-  
+
     if (error) {
       console.error('Error fetching data:', error);
       return;
     }
-  
+
     console.log('Fetched data:', data);
+    return data;
   }
+
   
