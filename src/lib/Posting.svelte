846b3732a5b51a0ae27c16e1b824b11c8602323a
cosmo4 <warner.luke0102@gmail.com>
2023-12-05 10:30:53 -0700

    fetchdata trying to connect to database

--- src/lib/Posting.svelte
+++ src/lib/Posting.svelte
@@ -1,4 +1,6 @@
-
+<script>
+  
+</script>
 <main>
     This is the posting page
 </main>
---
+++ src/lib/supabaseClient.mjs
@@ -0,0 +1,18 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey)
+const tableName = 'recipe'
+export async function fetchData() {
+    const { data, error } = await supabase.from(tableName).select('*');
+  
+    if (error) {
+      console.error('Error fetching data:', error);
+      return;
+    }
+  
+    console.log('Fetched data:', data);
+  }
+  
