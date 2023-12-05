c1786eb7fb4f885d5c22eace20be93e51af4300b
Cayden <114540789+caydenpark@users.noreply.github.com>
2023-12-05 10:01:16 -0700

    Posts skeleton complete

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -7,10 +7,10 @@
         <!-- If logged in, show profile and posting page -->
         <li><a href="#home">Home</a></li>
         <li><a href="#login">Login</a></li>
+        <li><a href="#posts">Posts</a></li>
         <!-- If not logged in, show login and signup else show logout -->
         <li><a href="#signup">Signup</a></li>
         <li><a href="#recipe">Recipe</a></li>
-
     </ul>
 </nav>
 
